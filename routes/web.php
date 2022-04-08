75e4cc7d0ef0ff2a7be6da20d116bf546562c0ea
Rui Almeida <rui.almeida011@gmail.com>
2022-04-08 15:29:01 +0100

    Laravavel/ui auth scaffold

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes(['register' => false]);
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
