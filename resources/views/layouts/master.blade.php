bec34542b3ca43b4aff4c5a99eae4e4f40304d31
Rui Almeida <rui.almeida011@gmail.com>
2022-04-20 16:01:28 +0100

    Added custom styles old way

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,6 +11,7 @@
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
         <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+        <link href="{{ asset('css/custom-styles.css') }}" rel="stylesheet" />
         
         @yield('css')
 
